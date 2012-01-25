03fb673ae80ac6eac726ca53ea7e1d912bf0a937
stevie <hotblack@disasterarea.org.uk>
2012-01-25 19:07:16 +0000

    Added spanners-webapp from Tapestry archetype

---
+++ spanners-webapp/src/main/java/org/dontpanic/spanners/webapp/pages/About.java
@@ -0,0 +1,6 @@
+package org.dontpanic.spanners.webapp.pages;
+
+public class About
+{
+
+}
---
+++ spanners-webapp/src/main/java/org/dontpanic/spanners/webapp/pages/Index.java
@@ -0,0 +1,14 @@
+package org.dontpanic.spanners.webapp.pages;
+
+import java.util.Date;
+
+/**
+ * Start page of application spanners-webapp.
+ */
+public class Index
+{
+	public Date getCurrentTime() 
+	{ 
+		return new Date(); 
+	}
+}
