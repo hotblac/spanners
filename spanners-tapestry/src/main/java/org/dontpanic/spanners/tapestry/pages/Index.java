1c365a74c274a4cfd0db31b38b46b52055a6fe34
stevie <hotblack@disasterarea.org.uk>
2012-01-28 22:02:43 +0000

    Demonstrated Tapestry / Spring / Hibernate working together. Tapestry can now fetch spanner from DAO.

--- spanners-tapestry/src/main/java/org/dontpanic/spanners/tapestry/pages/Index.java
+++ spanners-tapestry/src/main/java/org/dontpanic/spanners/tapestry/pages/Index.java
@@ -1,14 +1,32 @@
 package org.dontpanic.spanners.tapestry.pages;
 
-import java.util.Date;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.dontpanic.spanners.SpannersDAO;
+import org.springframework.context.ApplicationContext;
 
 /**
  * Start page of application spanners-tapestry.
  */
-public class Index
-{
-	public Date getCurrentTime() 
-	{ 
-		return new Date(); 
-	}
+public class Index {
+
+    @Inject
+    private ApplicationContext springContext;
+
+    /**
+     * We'd rather inject the spannersDAO bean using the @Inject annotation
+     * but Tapestry would get confused between two beans that implement this
+     * interface in our Spring context: the undecorated implementation and the
+     * transaction proxy. Solution is to inject the whole ApplicationContext
+     * and pick the required bean ourselves.
+     * @return spannersDAO transaction proxy bean
+     */
+    private SpannersDAO getSpannersDAO() {
+        return (SpannersDAO)springContext.getBean("spannersDAO");
+    }
+
+    public String getSpanner1() {
+        return getSpannersDAO().get(1).getName();
+    }
+
+
 }
