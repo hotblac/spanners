b79f6388540cc1d8c90a533178fd4d624badfd80
stevie <hotblack@disasterarea.org.uk>
2012-02-24 20:56:45 +0000

    Demonstrated simple Spring integration in Struts project

---
+++ spanners-struts/src/main/java/org/dontpanic/spanners/struts/SpannerDetailAction.java
@@ -0,0 +1,16 @@
+package org.dontpanic.spanners.struts;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * TODO document me
+ * User: Stevie
+ * Date: 24/02/12
+ */
+public class SpannerDetailAction extends ActionSupport {
+
+    public String execute() throws Exception {
+		return SUCCESS;
+	}
+
+}
---
+++ spanners-struts/src/main/webapp/spanners/SpannerDetail.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<html>
+<head>
+    <title>Welcome</title>
+    <link href="<s:url value="/css/examplecss"/>" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<h1>Body goes here!</h1>
+</body>
+</html>
