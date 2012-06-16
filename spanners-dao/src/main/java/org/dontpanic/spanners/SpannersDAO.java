dfcdb993b6c49a07bd3f2cf99b315aba16470495
stevie <hotblack@disasterarea.org.uk>
2012-06-16 20:09:18 +0000

    Demonstrated annotation based method security

--- spanners-dao/src/main/java/org/dontpanic/spanners/SpannersDAO.java
+++ spanners-dao/src/main/java/org/dontpanic/spanners/SpannersDAO.java
@@ -1,14 +1,18 @@
 package org.dontpanic.spanners;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+
 import java.util.List;
 
 /**
  * DAO for spanners
  */
+@PreAuthorize("hasRole('ROLE_EXCLUDE_ALL')")
 public interface SpannersDAO {
 
     public Spanner get(int id);
 
+    @PreAuthorize("hasRole('ROLE_VIEWER')")
     public List<Spanner> getAll();
 
     public int create(Spanner spanner);
