e43b8f4be703de286f03709d26b7a583033a7b9e
stevie <hotblack@disasterarea.org.uk>
2013-01-09 21:47:02 +0000

    Added login IT

--- spanners-struts/src/test/java/org/dontpanic/spanners/struts/SpannersStrutsIT.java
+++ spanners-struts/src/test/java/org/dontpanic/spanners/struts/SpannersStrutsIT.java
@@ -1,7 +1,9 @@
 package org.dontpanic.spanners.struts;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 import static org.junit.Assert.*;
@@ -23,6 +25,30 @@ public class SpannersStrutsIT {
         driver.get("http://localhost:9090/spanners-struts/");
         // Check that the login page is shown
         assertEquals("Login Page", driver.getTitle());
+    }
+
+
+    /**
+     * Test that we can login and view the main page
+     */
+    @Test
+    public void testLogin() {
+
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get("http://localhost:9090/spanners-struts/");
+
+        // Enter username and password
+        WebElement userField = driver.findElement(By.name("j_username"));
+        WebElement passwordField = driver.findElement(By.name("j_password"));
+        userField.sendKeys("smith");
+        passwordField.sendKeys("password");
+
+        // Click Login button
+        WebElement button = driver.findElement(By.name("submit"));
+        button.click();
+
+        // Check that user is logged in and spanners list page is shown
+        assertEquals("Display Spanners", driver.getTitle());
     }
 
 }
