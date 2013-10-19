0b51c45d46e47908c8aefffdc12912edd0f90496
stevie <hotblack@disasterarea.org.uk>
2013-10-19 16:48:07 +0100

    Added edit spanner controller

--- spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/controllers/EditSpannerController.java
+++ spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/controllers/EditSpannerController.java
@@ -5,26 +5,33 @@ import org.dontpanic.spanners.dao.SpannersDAO;
 import org.dontpanic.spanners.springmvc.exception.SpannerNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
- * Displays detail of a single spanner
+ * Controller for changing properties of an existing spanner
  * User: Stevie
- * Date: 13/10/13
+ * Date: 19/10/13
  */
 @Controller
-public class DetailSpannerController {
+public class EditSpannerController {
 
-    public static final String VIEW_DETAIL_SPANNER = "detailSpanner";
+    public static final String VIEW_EDIT_SPANNER = "editSpanner";
+    public static final String VIEW_UPDATE_SUCCESS = VIEW_EDIT_SPANNER;
+
     public static final String MODEL_SPANNER = "spanner";
 
     @Autowired private SpannersDAO spannersDAO;
 
-    @RequestMapping(value = "/detailSpanner", method = RequestMethod.GET)
-    public ModelAndView displayDetail(@RequestParam int id) throws SpannerNotFoundException {
+
+    /**
+     * Display the edit spanner page
+     */
+    @RequestMapping(value = "/editSpanner", method = RequestMethod.GET)
+    public ModelAndView displayPage(@RequestParam int id) throws SpannerNotFoundException {
 
         // Fetch the spanner
         Spanner spanner = spannersDAO.get(id);
@@ -33,6 +40,20 @@ public class DetailSpannerController {
             throw new SpannerNotFoundException(id);
         }
 
-        return new ModelAndView(VIEW_DETAIL_SPANNER, MODEL_SPANNER, spanner);
+        return new ModelAndView(VIEW_EDIT_SPANNER, MODEL_SPANNER, spanner);
     }
+
+
+    /**
+     * Accept form submission from edit spanner page
+     */
+    @RequestMapping(value = "/editSpanner", method = RequestMethod.POST)
+    public ModelAndView updateSpanner(@ModelAttribute Spanner spanner) {
+
+        spannersDAO.update(spanner);
+        return new ModelAndView(VIEW_UPDATE_SUCCESS);
+    }
+
+
+
 }
---
+++ spanners-mvc/src/test/java/org/dontpanic/spanners/springmvc/controllers/EditSpannerControllerTest.java
@@ -0,0 +1,86 @@
+package org.dontpanic.spanners.springmvc.controllers;
+
+import static org.dontpanic.spanners.springmvc.controllers.DetailSpannerController.MODEL_SPANNER;
+import static org.dontpanic.spanners.stubs.SpannersStubs.*;
+import static org.dontpanic.spanners.springmvc.controllers.EditSpannerController.*;
+
+import org.dontpanic.spanners.dao.Spanner;
+import org.dontpanic.spanners.dao.SpannersDAO;
+import org.dontpanic.spanners.springmvc.exception.SpannerNotFoundException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test for edit spanner controller
+ * User: Stevie
+ * Date: 19/10/13
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class EditSpannerControllerTest {
+
+    @Mock private SpannersDAO spannersDAO;
+    @InjectMocks private EditSpannerController controller = new EditSpannerController();
+
+    @Test
+    public void testDisplayPage() throws Exception {
+
+        // Stub behaviours - dao returns spanner detail
+        when(spannersDAO.get(SPANNER_ID)).thenReturn(SPANNER);
+
+        ModelAndView response = controller.displayPage(SPANNER_ID);
+        assertNotNull("no response", response);
+
+        // Assert that we forward to correct view
+        assertEquals("view", VIEW_EDIT_SPANNER, response.getViewName());
+
+        // Assert that spanner is in model
+        Spanner spannerInModel =  (Spanner)response.getModelMap().get(MODEL_SPANNER);
+        assertSpannerEquals(SPANNER, spannerInModel);
+    }
+
+
+    @Test
+    public void testSpannerNotFound() {
+
+        // Stub behaviours = spanner not found
+        when(spannersDAO.get(SPANNER_ID)).thenReturn(null);
+
+        // Request the page for the given spanner id - this should generate exception
+        try {
+            controller.displayPage(SPANNER_ID);
+            fail("Expected: SpannerNotFoundException");
+        } catch(SpannerNotFoundException notFoundEx) {
+            assertEquals("Exception spanner id", SPANNER_ID, notFoundEx.getSpannerId());
+        }
+    }
+
+
+    @Test
+    public void testUpdateSpanner() {
+
+        Spanner postData = stubSpanner(99);
+
+        ModelAndView response = controller.updateSpanner(postData);
+        assertNotNull("no response", response);
+
+        // Assert that we forward to correct view
+        assertEquals("view", VIEW_UPDATE_SUCCESS, response.getViewName());
+
+        // Verify that spanner was updated
+        ArgumentCaptor<Spanner> spannerArgumentCaptor = ArgumentCaptor.forClass(Spanner.class);
+        verify(spannersDAO).update(spannerArgumentCaptor.capture());
+        Spanner updatedSpanner = spannerArgumentCaptor.getValue();
+        assertSpannerEquals(postData, updatedSpanner);
+    }
+}
