6f3d61efd083a14d7df37ddf9ff808041f754831
stevie <hotblack@disasterarea.org.uk>
2013-11-28 20:17:33 +0000

    Added formatted date to home page

--- spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/controllers/DisplaySpannersController.java
+++ spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/controllers/DisplaySpannersController.java
@@ -5,11 +5,15 @@ import org.dontpanic.spanners.dao.SpannersDAO;
 import org.dontpanic.spanners.springmvc.exception.SpannerNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,8 +26,10 @@ public class DisplaySpannersController {
 
     public static final String VIEW_DISPLAY_SPANNERS = "displaySpanners";
     public static final String MODEL_ATTRIBUTE_SPANNERS = "spanners";
+    public static final String MODEL_ATTRIBUTE_DATE = "formattedDate";
 
     @Autowired private SpannersDAO spannersDAO;
+    private DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
 
     /**
      * Display all spanners
@@ -31,10 +37,18 @@ public class DisplaySpannersController {
     @RequestMapping(value = "/displaySpanners", method = RequestMethod.GET)
     public ModelAndView displaySpanners() {
 
+        ModelMap model = new ModelMap();
+
         // Load the spanners from database
         List<Spanner> spanners = spannersDAO.getAll();
+        model.put(MODEL_ATTRIBUTE_SPANNERS, spanners);
 
-        return new ModelAndView(VIEW_DISPLAY_SPANNERS, MODEL_ATTRIBUTE_SPANNERS, spanners);
+        // Format today's date
+        Date today = new Date();
+        String formattedDate = dateFormat.format(today);
+        model.put(MODEL_ATTRIBUTE_DATE, formattedDate);
+
+        return new ModelAndView(VIEW_DISPLAY_SPANNERS, model);
     }
 
 
