4dda00a4f702ee0669ac2615d4dbdbe309ac0f96
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2015-06-16 11:24:37 +0100

    Removed redundant ExceptionHandler to allow custom 403 (not authorized) error page to be shown

--- spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/error/UnauthorizedErrorController.java
+++ spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/error/UnauthorizedErrorController.java
@@ -1,17 +1,18 @@
 package org.dontpanic.spanners.springmvc.error;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Stevie on 15/06/2015.
  */
+@Controller
 public class UnauthorizedErrorController {
 
     public static final String CONTROLLER_URL = "403";
     public static final String VIEW_UNAUTHORIZED = "403";
 
-    @RequestMapping(value = CONTROLLER_URL, method = RequestMethod.GET)
+    @RequestMapping(CONTROLLER_URL)
     public String unauthorizedError() {
         return VIEW_UNAUTHORIZED;
     }
