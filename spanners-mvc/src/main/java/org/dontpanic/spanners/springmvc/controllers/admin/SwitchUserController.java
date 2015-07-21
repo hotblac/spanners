cf6b74aa947a7b4f1b1b1b724ba75d2e8ef7bfde
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2015-07-21 11:07:36 +0100

    Created switch user page (stub only)

---
+++ spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/controllers/admin/SwitchUserController.java
@@ -0,0 +1,22 @@
+package org.dontpanic.spanners.springmvc.controllers.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Controller that allows super users to switch user profiles
+ * Created by Stevie on 21/07/2015.
+ */
+@Controller
+public class SwitchUserController {
+
+    public static final String VIEW_SWITCH_USER = "admin/switchUser";
+
+    @RequestMapping(value = "/admin/switchUser", method = RequestMethod.GET)
+    public ModelAndView displayPage() {
+        return new ModelAndView(VIEW_SWITCH_USER);
+    }
+
+}
