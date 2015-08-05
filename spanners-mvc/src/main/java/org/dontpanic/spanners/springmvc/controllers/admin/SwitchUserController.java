82678970400d6ddae171baf910deee2a67889598
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2015-08-05 15:30:28 +0100

    Removed redundant controller for user switch page form submission.

--- spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/controllers/admin/SwitchUserController.java
+++ spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/controllers/admin/SwitchUserController.java
@@ -1,6 +1,5 @@
 package org.dontpanic.spanners.springmvc.controllers.admin;
 
-import org.dontpanic.spanners.springmvc.forms.SwitchUserForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,18 +14,10 @@ public class SwitchUserController {
 
     public static final String CONTROLLER_URL = "/admin/switchUser";
     public static final String VIEW_SWITCH_USER = "admin/switchUser";
-    public static final String USER_SWITCH_FILTER = "redirect:/admin/impersonate";
-    public static final String MODEL_SWITCH_USER = "switchUserForm";
 
     @RequestMapping(value = CONTROLLER_URL, method = RequestMethod.GET)
     public ModelAndView displayPage() {
-        SwitchUserForm form = new SwitchUserForm();
-        return new ModelAndView(VIEW_SWITCH_USER, MODEL_SWITCH_USER, form);
-    }
-
-    @RequestMapping(value = CONTROLLER_URL, method = RequestMethod.POST)
-    public String switchUser(SwitchUserForm form) {
-        return USER_SWITCH_FILTER + "?username=" + form.getUserName();
+        return new ModelAndView(VIEW_SWITCH_USER);
     }
 
 }
--- spanners-mvc/src/test/java/org/dontpanic/spanners/springmvc/controllers/admin/SwitchUserControllerTest.java
+++ spanners-mvc/src/test/java/org/dontpanic/spanners/springmvc/controllers/admin/SwitchUserControllerTest.java
@@ -1,12 +1,9 @@
 package org.dontpanic.spanners.springmvc.controllers.admin;
 
-import org.dontpanic.spanners.springmvc.forms.SwitchUserForm;
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
 
 import static org.dontpanic.spanners.springmvc.controllers.admin.SwitchUserController.VIEW_SWITCH_USER;
-import static org.dontpanic.spanners.springmvc.controllers.admin.SwitchUserController.USER_SWITCH_FILTER;
-import static org.dontpanic.spanners.springmvc.controllers.admin.SwitchUserController.MODEL_SWITCH_USER;
 import static org.junit.Assert.*;
 
 /**
@@ -28,24 +25,5 @@ public class SwitchUserControllerTest {
 
         // Assert that we forward to correct view
         assertEquals("view", VIEW_SWITCH_USER, response.getViewName());
-
-        // Assert that the form is available to the page
-        SwitchUserForm form = (SwitchUserForm)response.getModelMap().get(MODEL_SWITCH_USER);
-        assertNotNull("model", form);
-    }
-
-
-    @Test
-    public void testSwitchUser() {
-
-        // Submit the form
-        SwitchUserForm form = new SwitchUserForm();
-        form.setUserName(NEW_USER);
-        String response = controller.switchUser(form);
-        assertNotNull("no response", response);
-
-        // Assert that we forward to the switch user filte with URL params
-        String expectedForward = USER_SWITCH_FILTER + "?username=" + NEW_USER;
-        assertEquals("response", expectedForward, response);
     }
 }
