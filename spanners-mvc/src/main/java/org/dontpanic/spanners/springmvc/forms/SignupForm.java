038594ef589c374f0f9b77151cca86af56814110
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2015-12-28 21:35:50 +0000

    Added signup page

---
+++ spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/forms/SignupForm.java
@@ -0,0 +1,41 @@
+package org.dontpanic.spanners.springmvc.forms;
+
+import org.hibernate.validator.constraints.*;
+
+public class SignupForm {
+
+	private static final String NOT_BLANK_MESSAGE = "{notBlank.message}";
+	private static final String EMAIL_MESSAGE = "{email.message}";
+
+	@NotBlank(message = SignupForm.NOT_BLANK_MESSAGE)
+	private String name;
+	@NotBlank(message = SignupForm.NOT_BLANK_MESSAGE)
+	@Email(message = SignupForm.EMAIL_MESSAGE)
+	private String email;
+	@NotBlank(message = SignupForm.NOT_BLANK_MESSAGE)
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
