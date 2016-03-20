90e31f3e477b6735249c1a541086d1e16b79eedd
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2016-03-20 09:50:25 +0000

    Added stubbed getAll method to rest service

--- spanners-rest/src/main/java/org/dontpanic/spanners/rest/SpannersRestService.java
+++ spanners-rest/src/main/java/org/dontpanic/spanners/rest/SpannersRestService.java
@@ -4,14 +4,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by stevie on 18/03/16.
  */
 @RestController
+@RequestMapping("/spanners")
 public class SpannersRestService {
 
-    @RequestMapping("/spanners")
+    @RequestMapping(params = "id", method = GET)
     public String getSpanner(@RequestParam int id) {
         return "TODO";
+    }
+
+    @RequestMapping(method = GET)
+    public List<String> getAll() {
+        return Arrays.asList("TODO");
     }
 }
