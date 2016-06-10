6c7c2e16dc2589c52bac7554d2662625c3d88a54
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2016-06-10 21:04:40 +0100

    Removed redundant modules: spanners-dao, spanners-events, spanners-notifier and copy of old spanners-mvc3

--- spanners-mvc3/src/main/java/org/dontpanic/spanners/springmvc/config/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package org.dontpanic.spanners.springmvc.config;
-
-import org.springframework.context.annotation.*;
-
-@Configuration
-@ImportResource(value = "classpath:spring-security-context.xml")
-public class SecurityConfig {
-
-}
--- spanners-mvc3/src/main/webapp/WEB-INF/tiles/modal.jsp
+++
@@ -1,2 +0,0 @@
-<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles"%>
-<tiles:insertAttribute name="body" defaultValue="" />
