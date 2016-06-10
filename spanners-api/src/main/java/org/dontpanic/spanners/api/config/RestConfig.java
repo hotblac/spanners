2da54d70546449e0abf19af583ea6b8e4e4003a9
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2016-06-10 18:56:28 +0100

    Added spanner id field to spanners-api responses using recommended solution

--- spanners-api/src/main/java/org/dontpanic/spanners/api/config/RestConfig.java
+++ spanners-api/src/main/java/org/dontpanic/spanners/api/config/RestConfig.java
@@ -3,17 +3,17 @@ package org.dontpanic.spanners.api.config;
 import org.dontpanic.spanners.api.data.Spanner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 /**
  * Custom configuration for Spring Data REST
  * Created by stevie on 10/06/16.
  */
 @Configuration
-public class RestConfig extends RepositoryRestMvcConfiguration {
+public class RestConfig extends RepositoryRestConfigurerAdapter {
 
     @Override
-    protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Spanner.class);
     }
 }
