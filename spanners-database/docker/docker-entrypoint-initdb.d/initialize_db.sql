d4a5df450c829d060ec8cf2299b4df486fcdd608
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2016-07-02 21:01:17 +0100

    Added spanners-database project

---
+++ spanners-database/docker/docker-entrypoint-initdb.d/initialize_db.sql
@@ -0,0 +1,29 @@
+USE `spanners`;
+
+delimiter $$
+
+CREATE TABLE `spanner` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  `size` int(11) default NULL,
+  `owner` varchar(255) default NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+
+/*
+ * Spring Security users / authorities schema.
+ * Adapted from https://docs.spring.io/spring-security/site/docs/4.0.1.RELEASE/reference/html/appendix-schema.html
+ */
+create table users (
+    username varchar(50) not null primary key,
+    password varchar(60) not null,
+    enabled boolean not null
+) engine = InnoDb DEFAULT CHARSET=latin1$$
+
+create table authorities (
+    username varchar(50) not null,
+    authority varchar(50) not null,
+    foreign key (username) references users (username),
+    unique index authorities_idx_1 (username, authority)
+) engine = InnoDb DEFAULT CHARSET=latin1$$
