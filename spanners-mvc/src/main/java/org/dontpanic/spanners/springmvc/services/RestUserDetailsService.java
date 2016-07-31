16fd5ea861b4352abcbe782c554e2a5df67d4550
Stuart Leitch <sleitch@sabio.co.uk>
2016-07-31 21:08:31 +0100

    Implemented UserDetailsManager for user administration functions. Unit tested only

--- spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/services/RestUserDetailsService.java
+++ spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/services/RestUserDetailsService.java
@@ -6,12 +6,17 @@ import org.dontpanic.spanners.springmvc.domain.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.hateoas.hal.Jackson2HalModule;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -19,7 +24,7 @@ import org.springframework.web.client.RestTemplate;
  * Created by stevie on 14/07/16.
  */
 @Service
-public class RestUserDetailsService implements UserDetailsService {
+public class RestUserDetailsService implements UserDetailsManager {
 
     private RestTemplate restTemplate;
 
@@ -40,5 +45,44 @@ public class RestUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return restTemplate.getForObject("/{0}", User.class, username);
+    }
+
+    @Override
+    public void createUser(UserDetails userDetails) {
+        restTemplate.postForLocation("/", userDetails);
+    }
+
+    @Override
+    public void updateUser(UserDetails userDetails) {
+        restTemplate.put("/{0}", userDetails, userDetails.getUsername());
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        restTemplate.delete("/{0}", username);
+    }
+
+    @Override
+    public void changePassword(String username, String password) {
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        HttpEntity<User> requestEntity = new HttpEntity<User>(user);
+
+        restTemplate.exchange("/{0}", HttpMethod.PATCH, requestEntity, Void.class, username);
+    }
+
+    @Override
+    public boolean userExists(String username) {
+
+        try {
+            ResponseEntity<User> responseEntity = restTemplate.getForEntity("/{0}", User.class, username);
+            return HttpStatus.OK.equals(responseEntity.getStatusCode());
+        } catch (HttpClientErrorException e) {
+            // On HTTP 404 NOT FOUND or any HTTP error, assume user does not exist
+            return false;
+        }
     }
 }
--- spanners-mvc/src/test/java/org/dontpanic/spanners/springmvc/services/RestUserDetailsServiceTest.java
+++ spanners-mvc/src/test/java/org/dontpanic/spanners/springmvc/services/RestUserDetailsServiceTest.java
@@ -1,11 +1,13 @@
 package org.dontpanic.spanners.springmvc.services;
 
+import org.dontpanic.spanners.springmvc.domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,9 +15,10 @@ import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.test.web.client.ResponseCreator;
 
 import static org.junit.Assert.*;
-import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.*;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 /**
@@ -27,6 +30,8 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
 public class RestUserDetailsServiceTest {
 
     private static final String USERNAME = "jbloggs";
+    private static final String PASSWORD = "password123";
+    private static final boolean ENABLED = true;
     private static final MediaType APPLICATION_HAL_JSON = MediaType.valueOf("application/hal+json;charset=UTF-8");
 
     @Autowired
@@ -43,6 +48,79 @@ public class RestUserDetailsServiceTest {
 
         UserDetails userDetails = service.loadUserByUsername(USERNAME);
         assertNotNull(userDetails);
+    }
+
+
+    @Test
+    public void testCreateUser() throws Exception {
+
+        server.expect(requestTo("/")).andExpect(method(POST))
+                .andRespond(withStatus(HttpStatus.CREATED));
+
+        UserDetails user = new User(USERNAME, PASSWORD, ENABLED);
+        service.createUser(user);
+
+        server.verify();
+    }
+
+
+    @Test
+    public void testUpdateUser() throws Exception {
+
+        server.expect(requestTo("/" + USERNAME)).andExpect(method(PUT))
+                .andRespond(withStatus(HttpStatus.OK));
+
+        UserDetails user = new User(USERNAME, PASSWORD, ENABLED);
+        service.updateUser(user);
+
+        server.verify();
+    }
+
+
+    @Test
+    public void testDeleteUser() throws Exception {
+
+        server.expect(requestTo("/" + USERNAME)).andExpect(method(DELETE))
+                .andRespond(withStatus(HttpStatus.OK));
+
+        service.deleteUser(USERNAME);
+
+        server.verify();
+    }
+
+
+    @Test
+    public void testUserExists() throws Exception {
+
+        server.expect(requestTo("/" + USERNAME)).andExpect(method(GET))
+                .andRespond(withHalJsonResponse("/user_jbloggs_GET.txt"));
+
+        boolean userExists = service.userExists(USERNAME);
+        assertTrue(userExists);
+    }
+
+
+    @Test
+    public void testUserNotExists() throws Exception {
+
+        server.expect(requestTo("/" + USERNAME)).andExpect(method(GET))
+                .andRespond(withStatus(HttpStatus.NOT_FOUND));
+
+        boolean userExists = service.userExists(USERNAME);
+        assertFalse(userExists);
+    }
+
+
+    @Test
+    public void changePassword() throws Exception {
+
+        server.expect(requestTo("/" + USERNAME)).andExpect(method(PATCH))
+                .andRespond(withStatus(HttpStatus.OK));
+
+
+        service.changePassword(USERNAME, PASSWORD);
+
+        server.verify();
     }
 
 
