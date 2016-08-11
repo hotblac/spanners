b4cdeac4b79505bf83abcf256557c8787597ebe6
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2016-08-11 20:44:09 +0100

    Reinstate signup page

--- spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/controllers/SignupController.java
+++ spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/controllers/SignupController.java
@@ -1,8 +1,17 @@
 package org.dontpanic.spanners.springmvc.controllers;
 
+import org.dontpanic.spanners.springmvc.domain.User;
 import org.dontpanic.spanners.springmvc.forms.SignupForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Controller for the signup page. This page creates new user accounts.
@@ -12,11 +21,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class SignupController {
 
     public static final String CONTROLLER_URL = "/signup";
+	public static final String VIEW_SUCCESS = "redirect:/";
+
+    private static final boolean ENABLED = true;
+
+    /**
+     * UserDetailsManager provided by Spring Security allows CRUD operations on user accounts
+     */
+    @Autowired private UserDetailsManager userDetailsManager;
 
 	@RequestMapping(value = CONTROLLER_URL)
 	public SignupForm displayPage() {
 		return new SignupForm();
 	}
+
+    @RequestMapping(value = CONTROLLER_URL, method = RequestMethod.POST)
+    public String signup(@Valid @ModelAttribute SignupForm signupForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return null;
+        }
+
+        // Create the account
+        UserDetails userDetails = new User(signupForm.getName(), signupForm.getPassword(), ENABLED);
+        userDetailsManager.createUser(userDetails);
+
+        return VIEW_SUCCESS;
+    }
 
 
 }
