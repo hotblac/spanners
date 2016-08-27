7923e12fa2636a6bd6aa1531c7b193e41b67ce8e
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2016-08-27 21:20:27 +0100

    Autoconfigure mockMvc by annotation

--- spanners-api/src/test/java/org/dontpanic/spanners/api/data/RestApiTest.java
+++ spanners-api/src/test/java/org/dontpanic/spanners/api/data/RestApiTest.java
@@ -1,22 +1,20 @@
 package org.dontpanic.spanners.api.data;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.dontpanic.spanners.api.stubs.SpannerBuilder.aTestSpanner;
 import static org.hamcrest.Matchers.isEmptyString;
 import static org.hamcrest.Matchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 /**
  * Tests on the REST API exposed by this application.
@@ -25,21 +23,15 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @WebAppConfiguration
+@AutoConfigureMockMvc
 public class RestApiTest {
 
-    private MockMvc mockMvc;
-
     @Autowired
-    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
 
     @Autowired
     private SpannerRepository repository;
 
-
-    @Before
-    public void setup() throws Exception {
-        this.mockMvc = webAppContextSetup(webApplicationContext).build();
-    }
 
     /**
      * By default, Spring Data REST does not expose database ids.
