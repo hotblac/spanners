64cf6fac7e85edd1d1a6f1a9c3fce178d93d6f78
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2016-09-08 10:24:41 +0100

    Removed redundant SpringBootServletInitializer

--- spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/Application.java
+++ spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/Application.java
@@ -2,21 +2,15 @@ package org.dontpanic.spanners.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 /**
  * Spring Boot application entry point for Spanners MVC
  * Created by stevie on 06/06/16.
  */
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
 }
