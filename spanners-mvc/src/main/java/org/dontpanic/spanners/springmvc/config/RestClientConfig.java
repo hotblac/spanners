f184642cb89149ddfcac9ae1292cb67766d4da8e
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2016-09-09 12:22:39 +0100

    Allow localhost configuration of spanners-mvc without Eureka name resolution

--- spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/config/RestClientConfig.java
+++ spanners-mvc/src/main/java/org/dontpanic/spanners/springmvc/config/RestClientConfig.java
@@ -1,20 +1,39 @@
 package org.dontpanic.spanners.springmvc.config;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClientHttpRequestFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
 
 /**
  * Configure the Rest client
  * Created by stevie on 09/09/16.
  */
 @Configuration
+@EnableDiscoveryClient
 public class RestClientConfig {
 
+    private Logger logger = Logger.getLogger(RestClientConfig.class);
+
+    @Autowired(required = false)
+    private RibbonClientHttpRequestFactory ribbonRequestFactory;
+
+    /**
+     * Use Ribbon load balancer to resolve service endpoints
+     */
     @Bean
-    public RestTemplateBuilder restTemplateBuilder(ClientHttpRequestFactory requestFactory) {
-        return new RestTemplateBuilder().requestFactory(requestFactory);
+    public RestTemplateBuilder restTemplateBuilder() {
+
+        RestTemplateBuilder builder = new RestTemplateBuilder();
+        if (ribbonRequestFactory != null) {
+            builder = builder.requestFactory(ribbonRequestFactory);
+        } else {
+            logger.warn("Ribbon Http client disabled. Service endpoints will not be resolved by Eureka.");
+        }
+        return builder;
     }
 
 }
