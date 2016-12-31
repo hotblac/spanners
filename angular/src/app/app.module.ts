f7f4305b58846bc8e4758c005cbec6ebd41b482e
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2016-12-31 16:50:46 +0000

    Add warning regarding missing login functionality

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { CollapseModule } from 'ng2-bootstrap';
+import { CollapseModule, ModalModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
 import { DisplaySpannersComponent } from './display-spanners/display-spanners.component';
@@ -38,6 +38,7 @@ const appRoutes: Routes = [
     FormsModule,
     HttpModule,
     CollapseModule,
+    ModalModule.forRoot(),
     RouterModule.forRoot(appRoutes)
   ],
   providers: [SpannersService],
